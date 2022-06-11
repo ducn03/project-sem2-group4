01e20544952046a6e64d95c7c58cef4bb3949d12
ducn03 <94164852+ducn03@users.noreply.github.com>
2022-06-11 23:01:15 +0700

    Add files via upload

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,40 @@
+<hr>
+<footer>
+     <div class="container">
+         <!--Bắt Đầu Nội Dung Giới Thiệu-->
+         <div class="noi-dung about">
+             <h2>About us</h2>
+             <ul class="social-icon">
+                 <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                 <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                 <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                 <li><a href=""><i class="fa fa-youtube"></i></a></li>
+             </ul>
+         </div>
+         <!--Kết Thúc Nội Dung Giới Thiệu-->
+         <!--Bắt Đầu Nội Dung Đường Dẫn-->
+         <div class="noi-dung links">
+             <h2>Link</h2>
+             <ul>
+                 <li><a href="#">Home page</a></li>
+                 <li><a href="#">About us</a></li>
+                 <li><a href="#">Communications</a></li>
+                 <li><a href="#">Service</a></li>
+                 <li><a href="#">Policy conditions</a></li>
+             </ul>
+         </div>
+         <!--Kết Thúc Nội Dung Đường Dẫn-->
+         <!--Bắt Đâu Nội Dung Liên Hệ-->
+         <div class="noi-dung contact">
+             <h2>Contact info</h2>
+             <ul class="info">
+                 <li>
+                     <span><i class="fa fa-map-marker"></i></span>
+                     <span>580 August Revolution, Ho Chi Minh City, Vietnam</span>
+                 </li>
+                 
+             </ul>
+         </div>
+         <!--Kết Thúc Nội Dung Liên Hệ-->
+     </div>
+ </footer>
