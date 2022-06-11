8a1688f48fb69c3dc36a7488a026c4d26927db42
ducn03 <94164852+ducn03@users.noreply.github.com>
2022-06-11 23:01:47 +0700

    Add files via upload

---
+++ storage/framework/views/770b764f824025badcfc728b79e63014c8bd335e.php
@@ -0,0 +1,175 @@
+
+<?php $__env->startSection('title', 'OrganicFood'); ?>
+<?php $__env->startSection('content'); ?>
+<nav class="container">
+    <div class="bg-white ">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="navbar-list">
+                    <div class="subnav-header">
+                        <button class="subnavbtn" disabled><i class="fa fa-bars"></i> List of products</button>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Vegetable<i class="fa fa-caret-square-o-right"></i></button>
+                        <div class="subnav-content">
+                            <a href="#company">Organic leafy vegetables</a>
+                            <a href="#team">Organic fruits and vegetables</a>
+                            <a href="#careers">Fresh Mushrooms</a>
+                        </div>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Fresh food<i class="fa fa-caret-square-o-right"></i></button>
+                        <div class="subnav-content">
+                            <a href="#bring">Organic Pork</a>
+                            <a href="#deliver">Organic beef</a>
+                            <a href="#package">Poultry - Eggs</a>
+                            <a href="#express">Fish and Seafood</a>
+                            <a href="#express">Dry and a sunny</a>
+                        </div>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Dry food <i class="fa fa-caret-square-o-right"></i></button>
+                        <div class="subnav-content">
+                            <a href="#link1">Organic nuts</a>
+                            <a href="#link2">Organic cereals</a>
+                            <a href="#link3">Organic rice</a>
+                            <a href="#link4">Organic noodles and noodles</a>
+                            <a href="#link1">Confectionery and chocolate</a>
+                            <a href="#link2">Other dry goods</a>
+                            <a href="#link3">Baking material</a>
+                        </div>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Convenience food</button>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Organic fruit <i class="fa fa-caret-square-o-right"></i></button>
+                        <div class="subnav-content">
+                            <a href="#link1">Vietnamese fruit</a>
+                            <a href="#link2">Imported fruit</a>
+                            <a href="#link3">Dried fruit - Frozen</a>
+                        </div>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Spices and ingredients <i class="fa fa-caret-square-o-right"></i></button>
+                        <div class="subnav-content">
+                            <a href="#link1">Spice</a>
+                            <a href="#link2">Materials</a>
+                            <a href="#link3">honey</a>
+                        </div>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Organic drinks <i class="fa fa-caret-square-o-right"></i></button>
+                        <div class="subnav-content">
+                            <a href="#link1">Organic tea</a>
+                            <a href="#link2">Organic coffee</a>
+                            <a href="#link3">Organic juices</a>
+                        </div>
+                    </div>
+                    <div class="subnav">
+                        <button class="subnavbtn">Butter - Milk <i class="fa fa-caret-square-o-right"></i></button>
+                        <div class="subnav-content">
+                            <a href="#link1">Nut milk</a>
+                            <a href="#link2">Fresh milk</a>
+                            <a href="#link3">Yogurt</a>
+                            <a href="#link1">Butter and Cheese</a>
+                            <a href="#link2">Condensed milk</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-9">
+
+                <div style="border-radius: 15px;" id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="<?php echo e(asset('img/carousel1.jpg')); ?>" alt="" style="width:100%; height:348px; border-radius: 15px;">
+                        </div>
+
+                        <div class="item">
+                            <img src="<?php echo e(asset('img/carousel2.jpg')); ?>" alt="" style="width:100%; height:348px; border-radius: 15px;">
+                        </div>
+
+                        <div class="item">
+                            <img src="<?php echo e(asset('img/carousel3.jpg')); ?>" alt="" style="width:100%; height:348px; border-radius: 15px;">
+                        </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a style="border-radius: 15px;" class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a style="border-radius: 15px;" class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</nav>
+
+    <br><br><div style="border: 1px solid; border-color:#828282;"></div><br><br>
+
+<section class="ftco-section ftco-no-pb">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-2 col-md-4 ">
+						<div class="sort w-100 text-center ftco-animate">
+							<div class="img home-img1" ></div>
+							<h3>Vegetables</h3>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-4 ">
+						<div class="sort w-100 text-center ftco-animate">
+							<div class="img" style="background-image: url();"></div>
+							<h3>Gin</h3>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-4 ">
+						<div class="sort w-100 text-center ftco-animate">
+							<div class="img" style="background-image: url(images/kind-3.jpg);"></div>
+							<h3>Rum</h3>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-4 ">
+						<div class="sort w-100 text-center ftco-animate">
+							<div class="img" style="background-image: url(images/kind-4.jpg);"></div>
+							<h3>Tequila</h3>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-4 ">
+						<div class="sort w-100 text-center ftco-animate">
+							<div class="img" style="background-image: url(images/kind-5.jpg);"></div>
+							<h3>Vodka</h3>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-4 ">
+						<div class="sort w-100 text-center ftco-animate">
+							<div class="img" style="background-image: url(images/kind-6.jpg);"></div>
+							<h3>Whiskey</h3>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script-section'); ?>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\xampp\htdocs\project-sem2\resources\views/home/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b53907203bfd6bf2c7ec8786958e16dbf7814ac5.php
@@ -0,0 +1,35 @@
+<nav style="background-color: #00CC66; border:none;" id="test-navbar"  class="test-navbar">
+  <div class="container-fluid">
+    <div style="color: white;" class="navbar-header">
+      <br>
+    <span><i class="fa fa-phone"></i></span>
+    <a style="color:white" href="#">+84 123 456 789</a>&nbsp;&nbsp;&nbsp;
+    <span><i class="fa fa-envelope"></i></span>
+    <a style="color:white" href="#">diachiemail@gmail.com</a></p>
+  
+    </div>
+    
+    <ul class="nav-hover navbar-nav navbar-right">
+      <li><a class="navbar_login" href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a class="navbar_login" href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+<!--main-navbar-->
+<nav style="background-color: #fff; border:none;" id="main-navbar" class="main-navbar">
+  <div class="container-fluid">
+    <div style="color: white;" class="navbar-main-header">
+      <img src="<?php echo e(asset('img/logo.png')); ?>" alt="" height="45px">
+      <div></div>
+    </div>
+    
+    <ul class="nav-main-hover navbar-nav navbar-right">
+      <li><a class="navbar_main" href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+      <li><a class="navbar_main" href="#"><span class=""></span>About</a></li>
+      <li><a class="navbar_main" href="#"><span class=""></span>Contact</a></li>
+      <li><a class="navbar_main" href="#"><span class="glyphicon glyphicon-shopping-cart"></span>cart</a></li>
+    </ul>
+  </div>
+</nav>
+<?php /**PATH D:\xampp\htdocs\project-sem2\resources\views/layout/nav-bar.blade.php ENDPATH**/ ?>
